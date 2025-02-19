e6feecbedab0818b50af28cb00e01a8835263946
jawahereng <jawaherbhd15@gmail.com>
2025-02-19 16:56:18 +0300

    Update Inspector.java

--- Inspector.java
+++ Inspector.java
@@ -5,13 +5,14 @@ public class Inspector extends Staff {
 	private int numOfInspections;
 	
 	public Inspector (int hours, String name, int yearOfEx,int numOfInspections) {
-      super(hours,name,yearOfEx);
-      this.numOfInspections=numOfInspections; 
-    }
+     		super(hours,name,yearOfEx);
+     	        this.numOfInspections=numOfInspections; 
+  	  }
     
-    public double Salary(int h) {
+ 	public double Salary(int h) {
 		return super.Salary+(numOfInspections*200);
-		
-    	
 	}
+	public void setNumOfInspections(int numOfInspections){this.numOfInspections=numOfInspections;}
+	public int getNumOfInspections(){reetuyrn numOfInspections;}
+ 
 }
