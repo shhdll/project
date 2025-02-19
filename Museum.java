06eecdb08655082f7ffe7d42c9d26b48729aa8d7
shhdll <102389176+shhdll@users.noreply.github.com>
2025-02-19 14:28:25 +0300

    Update Museum.java

--- Museum.java
+++ Museum.java
@@ -24,30 +24,35 @@ public boolean addArt(Art a) { //aggregation
 	if(numOfArts >= ArtList.length)
 		return false;
 	ArtList[numOfArts++]=a;
-	if(a instanceOf(Paintings))
+	
+	if(a instanceof(Paintings)) //check if art is a painting
 		numOfPaintings++;
 
-	if(a instanceOf(Sculptures))
+	if(a instanceof(Sculptures)) //check if art is a sculpture
 		numOfSculpturess++;
+	
 	return true;
 	    }
 	 
  public boolean removeArt(Art a) { //aggregation
-	 if(numOfArt>0){
+	 if(numOfArts>0){
 		 for(int i=0 ; i<ArtList.length ; i++)
-			 if(ArtList[i].grtId()==a.getId()){
+			 if(ArtList[i].getId()==a.getId()){
 				 ArtList[i]=ArtList[numOfArts-1];
-					 ArtList[--numOfArt]=null;
+					 ArtList[--numOfArts]=null;
 			 }
-		 if(a instanceof(Paintings))
+		 
+	if(a instanceof(Paintings)) //check if art is a painting
 			 numOfPaintings--;
-		 if(a instanceOf(Sculptures))
+		 
+	if(a instanceof(Sculptures))  //check if art is a sculpture
 		numOfSculpturess--;
+		 
 		 return true;
 	    }
 	 return false;
  }
- public Art searchArt(int id) { //aggreigation
+ public Art searchArt(int id) { //aggregation
 	  for(int i=0 ; i<ArtList.length ; i++)
 			 if(ArtList[i].getId()==id)
 				 return ArtList[i];
@@ -64,16 +69,18 @@ public boolean addStaff(Staff e) { //composition
 
 public boolean removeStaff(Staff e) { //composition
 	if(numOfStaff >0)
-		for(int i=0 ; i<StaffList.length : i++){
+		for(int i=0 ; i<StaffList.length ; i++){
 			if(StaffList[i].getName() == e.getName()){
 				StaffList[i]=StaffList[numOfStaff-1];
-			StaffList[--numOfStaff]= null; 
-		}
-		}
-			
-	    }
+				StaffList[--numOfStaff]= null; 
+			}
+		}	
+	return false;
+	 }
 
-public Employee searchStaff(String name) { //composition 
-	    }
-
+public Staff searchStaff(String name) { //composition 
+	for(int i=0; i<StaffList.length ; i++)
+		if(StaffList[i].getName() == name)
+			return StaffList[i];
+	}
 }
