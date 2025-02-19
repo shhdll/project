2faf87317af3d59a66f1c4a7f07aa92cea29cefd
jawahereng <jawaherbhd15@gmail.com>
2025-02-19 16:58:26 +0300

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -3,15 +3,18 @@ package project;
 public class Manager extends Staff {
 	
 	private int officeNum;
-
-    public Manager(int hours, String name, int officeNum) {
-      super(hours,name);
-      this.officeNum=officeNum; 
-    }
+	public Manager(int hours, String name, int officeNum) {
+		super(hours,name);
+		this.officeNum=officeNum; 
+	}
    
-    public double Salary(int h) {
-    	return 1000*hours;
-    }
+	public double Salary(int h) {
+    		return 1000*hours;
+	}
+
+	public void setOfficeNum(int officeNum){this.officeNum=officeNum;}
+	public int getOfficeNum(){return officeNum;}
+	
 }
 
 
