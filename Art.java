de0bac81c069cba813dcb91554d5d4e7def469dc
shhdll <102389176+shhdll@users.noreply.github.com>
2025-02-20 12:25:48 +0300

    Update Art.java

--- Art.java
+++ Art.java
@@ -1,26 +1,33 @@
 package project;
 
 public class Art {
-	
-	protected int id;
-	protected int date;
-	protected String artist;
 
-public Art(int id, int date, String artist) {	
-	this.id=id;
-	this.date=date;
-	this.artist=artist;
-    }
-    
- public String toString() {
-	return " ID : "+id+" Date : "+date+" Artist : "+artist;
+    protected int id;
+    protected int date;
+    protected String artist;
+    protected String name;
+
+    public Art(int id, int date, String artist, String name) {
+        this.id = id;
+        this.date = date;
+        this.artist = artist;
+        this.name = name;
     }
 
-public void setId(int id){this.id=id;}
-public void setDate(int date){this.date=date;}
-public void serArtist(String artist){this.artist=artist;}
-public int getId(){return id;}
-public int getDate(){return date;}
-public String getArtist(){return artist;}
+    public String toString() {
+        return "ID: " + id + " Date: " + date + " Artist: " + artist+ " name: "+name;
+    }
 
+    // Getters and Setters
+    public void setId(int id) { this.id = id; }
+
+    public void setDate(int date) { this.date = date; }
+
+    public void setArtist(String artist) { this.artist = artist; }
+
+    public int getId() { return id; }
+
+    public int getDate() { return date; }
+
+    public String getArtist() { return artist; }
 }
