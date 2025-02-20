3f85aeaac2777a12057a3a04899f33a5bc08fc8d
shhdll <102389176+shhdll@users.noreply.github.com>
2025-02-20 12:29:27 +0300

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,19 +1,23 @@
 package project;
 
 public abstract class Staff {
-	
-	 protected int hours;
-	 protected String name;
 
-public Staff(int hours, String name) {	
-	this.hours=hours;
-	this.name = name;
-	}
-	
-public abstract double  Salary(int h);
+    protected int hours;
+    protected String name;
 
-public int getHours(){return hours;}
-public void setHours(int hours){this.hours=hours;}
-public String getName(){return name;}
-public void setName(String name){this.name=name;}
+    public Staff(int hours, String name) {
+        this.hours = hours;
+        this.name = name;
+    }
+
+    public abstract double Salary(int h);
+
+    // getters, setters
+    public int getHours() { return hours; }
+
+    public void setHours(int hours) { this.hours = hours; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
 }
