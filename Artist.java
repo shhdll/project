1646fd6832afb6c66c4782741f459ddb262d9c78
shhdll <102389176+shhdll@users.noreply.github.com>
2025-02-20 12:26:06 +0300

    Update Artist.java

--- Artist.java
+++ Artist.java
@@ -1,18 +1,19 @@
 package project;
 
 public class Artist extends Staff {
-   
-	protected int yearOfEx;
-
- public Artist(int hours, String name, int yearOfEx) {
-      super(hours,name);
-      this.yearOfEx=yearOfEx; 
-    }
     
- public double Salary(int h) {
-   return (hours*500)+(100*yearOfEx);
+    protected int yearOfEx;
+
+    public Artist(int hours, String name, int yearOfEx) {
+        super(hours, name);
+        this.yearOfEx = yearOfEx;
     }
-public void setYearOfEx(int yearOfEx){this.yearOfEx=yearOfEx;}
-public int getYearOfEx(){return yearOfEx;}
-	
+
+    public double Salary(int h) {
+        return (hours * 500) + (100 * yearOfEx);
+    }
+
+    // getters, setters
+    public void setYearOfEx(int yearOfEx) { this.yearOfEx = yearOfEx; }
+    public int getYearOfEx() { return yearOfEx; }
 }
