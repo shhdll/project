9f162fe6bd8f44267f128f088daf464d404b9345
shhdll <102389176+shhdll@users.noreply.github.com>
2025-02-20 12:28:41 +0300

    Update Paintings.java

--- Paintings.java
+++ Paintings.java
@@ -1,20 +1,20 @@
 package project;
 
 public class Paintings extends Art {
-	
-	private String typeOfPainting;
-
-	public Paintings(int id,String date,String artist, String typeOfPainting ){
-		super(id , date , artist);
-		this.typeOfPainting = typeOfPainting;
-		
-	}
-	    
-public String toString() {
-	return super.toString+" Type of painting: "+typeOfPainting;
-	    }
-	public String getTypeOfPainting() {return typeOfPainting;}
-
     
-    public void setTypeOfPainting(String typeOfPainting){this.typeOfPainting = typeOfPainting;}
+    private String typeOfPainting;
+
+    public Paintings(int id, String date, String artist, String typeOfPainting, String name) {
+        super(id, date, artist, name);
+        this.typeOfPainting = typeOfPainting;
+    }
+
+    public String toString() {
+        return super.toString() + " Type of painting: " + typeOfPainting;
+    }
+
+    // getters, setters
+    public String getTypeOfPainting() { return typeOfPainting; }
+
+    public void setTypeOfPainting(String typeOfPainting) { this.typeOfPainting = typeOfPainting; }
 }
