d867e2d77196b360fb830be87619ed165119cfbc
jawaher <jawaherbhd15@gmail.com>
2025-02-25 00:04:09 +0300

    edit#3

--- Paintings.java
+++ Paintings.java
@@ -8,7 +8,7 @@ public class Paintings extends Art {
         super(id, date, artist, name);
         this.typeOfPainting = typeOfPainting;
     }
-
+    @Override
     public String toString() {
         return super.toString() + " Type of painting: " + typeOfPainting;
     }
--- Sculptures.java
+++ Sculptures.java
@@ -8,7 +8,7 @@ public class Sculptures extends Art {
         super(id, date, artist, name);
         this.typeOfSculp = typeOfSculp;
     }
-
+    @Override
     public String toString() {
         return super.toString() + " Type of sculpture: " + typeOfSculp; 
     }
