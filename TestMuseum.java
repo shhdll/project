c8de897bfbf55ed883d187a0e0f16b9c49212d99
jawaher <jawaherbhd15@gmail.com>
2025-02-25 18:40:51 +0300

    ADD STTAFF

--- TestMuseum.java
+++ TestMuseum.java
@@ -32,7 +32,8 @@ public class TestMuseum {
             System.out.println("4- Remove staff");
             System.out.println("5- Search art");
             System.out.println("6- Search staff");
-            System.out.println("7- Exit");
+            System.out.println("7- display all information");
+            System.out.println("8- Exit");
             
             choice = input.nextInt();
             
@@ -87,11 +88,68 @@ public class TestMuseum {
                         
                     }
                     else 
-                        System.out.println("Invalid");
+                        System.out.println("Invalid ");
                     
                 		                	
                     break;
-                case 2:
+                case 2: 
+                    System.out.println("Enter M for Manager, A for Artist , I for Inspector");
+                    char staffChoice = input.next().charAt(0);
+                    input.nextLine(); // clean garbage
+                    switch(staffChoice){
+                        case 'M': case 'm':
+                        System.out.print("Enter Hours: ");
+                        int hours = input.nextInt();
+                        System.out.print("Enter Name: ");
+                        String name = input.nextLine();
+                        input.nextLine();
+                        System.out.print("Enter Office Number: ");
+                        int ofnum = input.nextInt();
+                        
+                        Manager objM = new Manager(hours,name,ofnum);
+
+                        if (m1.addStaff(objM)) 
+                            System.out.println("Manager added successfully :)");
+                         else 
+                            System.out.println("looks like the museum is full :(");
+                            break;
+                        case 'A': case 'a':
+
+                        System.out.print("Enter Hours: ");
+                        int hours2 = input.nextInt();
+                        System.out.print("Enter Name: ");
+                        String name2 = input.nextLine();
+                        input.nextLine();
+                        System.out.print("Enter yaers of experience : ");
+                        int yearOfEx = input.nextInt();
+
+                        Artist a = new Artist(hours2, name2, yearOfEx);
+                        if (m1.addStaff(a)) 
+                        System.out.println("Artist added successfully :)");
+                        else 
+                        System.out.println("looks like the museum is full :(");
+                        break;
+
+                        case 'I':case'i':
+                        System.out.print("Enter Hours: ");
+                        int hours3 = input.nextInt();
+                        System.out.print("Enter Name: ");
+                        String name3 = input.nextLine();
+                        input.nextLine();
+                        System.out.print("Enter yaers of experience : ");
+                        int yearOfEx1 = input.nextInt();
+                        System.out.print("Enter number Of Inspections : ");
+                        int Inspections = input.nextInt();
+
+                        Inspector objI = new Inspector(hours3,name3,yearOfEx1,Inspections);
+
+                        if (m1.addStaff(objI)) 
+                            System.out.println("Manager added successfully :)");
+                         else 
+                            System.out.println("looks like the museum is full :(");
+                        break;
+                    }
+
                 	
                     break;
                 case 3:   //remove art
@@ -120,6 +178,8 @@ public class TestMuseum {
                 	
                     break;
                 case 7:
+                        break;
+                case 8:
                 	
                     b = false;
                     System.out.print("Thank you for using our system!");
